a18cd1f52344858b04cbc0f81b0c89d433743ca3
= <abisekh.work@gmail.com>
2022-04-02 21:44:07 +0545

    update graphql query hook for each char

---
+++ src/hooks/useCharacter.js
@@ -0,0 +1,26 @@
+import { useQuery, gql } from "@apollo/client";
+
+const GET_CHARACTER = gql`
+  query GetCharacter($id: ID!) {
+    character(id: $id) {
+        id
+        name
+        image
+        gender
+        episode {
+          name
+          episode
+        }
+    }
+  }
+`;
+
+export const useCharacter = (id) => {
+  const { loading, data, error } = useQuery(GET_CHARACTER, {
+    variables: {
+      id,
+    },
+  });
+
+  return { loading, data, error };
+};
